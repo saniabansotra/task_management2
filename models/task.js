cdd5891f20a8a06096610dd9742f6202a2442a4a
Sania Bansotra <saniabansotra@gmail.com>
2023-07-16 22:15:34 +0530

    message

--- models/task.js
+++ models/task.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 const task_schema = new mongoose.Schema(
   {
-    task_name: String,
-    task_description: String,
-    task_date: Date,
+    task_name: { type: String },
+    task_description: { type: String },
+    task_date: { type: Date },
+    task_status: {
+      type: String,
+      enum: ["To Do", "In Progress", "Complete"],
+    },
   },
   { timestamp: true }
 );
